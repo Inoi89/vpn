5a2eb1af0f43191e58c816ffc7c3daf68278ded0
Serafim <49989163+Inoi89@users.noreply.github.com>
2025-07-10 15:52:28 +0300

    Add observable logger provider and bind logs to UI

--- UI/Program.cs
+++ UI/Program.cs
@@ -2,6 +2,10 @@ using System;
 using Avalonia;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Collections.ObjectModel;
+using VpnClient.Core.Models;
+using VpnClient.Infrastructure.Logging;
 using VpnClient.Core.Interfaces;
 using VpnClient.Infrastructure.Services;
 using VpnClient.UI.ViewModels;
@@ -17,12 +21,19 @@ class Program
     {
         var builder = Host.CreateApplicationBuilder(args);
         builder.Services.AddLogging();
+        builder.Services.AddSingleton<ObservableCollection<LogEntry>>();
+        builder.Services.AddSingleton<ObservableLoggerProvider>();
         builder.Services.AddSingleton<IWintunService, WintunService>();
         builder.Services.AddSingleton<IVpnService, VpnService>();
         builder.Services.AddSingleton<MainWindow>();
         builder.Services.AddSingleton<MainWindowViewModel>();
 
         var host = builder.Build();
+
+        var loggerFactory = host.Services.GetRequiredService<ILoggerFactory>();
+        var provider = host.Services.GetRequiredService<ObservableLoggerProvider>();
+        loggerFactory.AddProvider(provider);
+
         Services = host.Services;
 
         BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
